612808e6f70335a06b467b458bf1febea68a31a2
Trevor Pilgrim <pilg5971@stthomas.edu>
2022-02-15 20:47:46 -0600

    Added Navigation & Mobile Menu

--- js/main.js
+++ js/main.js
@@ -2,7 +2,23 @@
 
 
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+// Navigation Toggle
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+// Close Navigation
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
