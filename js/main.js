b0fcda7f775b7bca470ed9e45a03c64f45ba0f83
Trevor Pilgrim <pilg5971@stthomas.edu>
2022-02-15 21:24:46 -0600

    Home Section V1

--- js/main.js
+++ js/main.js
@@ -20,11 +20,27 @@ if(navClose){
   })
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*=============== NAVIGATION -> REMOVE MENU ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+  const header = document.getElementById('header')
+  if(this.scrollY >= 50){
+    header.classList.add('scroll-header')
+  }else{
+    header.classList.remove('scroll-header')
+  }
+}
 
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
 
